c1fa15a6a2e7dbf2720e3d31b9151bedfc69893c
LIMON <limonfpi408@gmail.com>
2025-06-16 21:45:15 +0600

    old seeder file moved

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,35 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(OrganizationInfoTableSeeder::class);
-        $this->command->info("OrganizationInfo table seeded");
-
-        $this->call(DesignationTableSeeder::class);
-        $this->command->info("Designation table seeded");
-
-        $this->call(EmployeeTableSeeder::class);
-        $this->command->info("Employees table seeded");
-
-        $this->call(UserTableSeeder::class);
-        $this->command->info("User table seeded");
-
-        $this->call(DriverInfoTableSeeder::class);
-        $this->command->info("DriverInfo table seeded");
-
-        $this->call(VehicleSetupTableSeeder::class);
-        $this->command->info("VehicleSetup table seeded");
-
-        $this->call(LogBookTableSeeder::class);
-        $this->command->info("logBooks table seeded");
-
-        $this->call(FuelOilTableSeeder::class);
-        $this->command->info("FuelOil table seeded");
-
-        $this->call(MeterTableSeeder::class);
-        $this->command->info("Meter table seeded");
-
-        $this->call(RepairsTableSeeder::class);
-        $this->command->info("Repairs table seeded");
+        # call all seeders
 
     }
 }
